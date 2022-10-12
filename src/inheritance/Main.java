02486dca49ca2188cbcc9bd7b30563c802c2700a
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-10-12 11:46:39 +0800

    Added Inheritance package

---
+++ src/inheritance/Main.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+        System.out.println("Animal Name: " + animal.getName());
+
+        Dog dog = new Dog("Yorkshire Terrier", 8, 20, 2, 4, 1, 20, "Long Silky" );
+        System.out.println("Dog name: " + dog.getName());
+    }
+}
